58e876827b1c61eb37255952cde229d4599ab21b
Artem Medvedev <aaartem08@mail.ru>
2015-10-09 17:16:33 +0300

    Creaated base project structure

--- app/src/main/java/com/example/aaart/reminder/MainActivity.java
+++ app/src/main/java/com/example/aaart/reminder/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.aaart.reminder;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iniToolbar();
+    }
+
+    private void iniToolbar() {
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        mToolBar.setTitle(R.string.app_name);
+        mToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        mToolBar.inflateMenu(R.menu.menu);
     }
 }
